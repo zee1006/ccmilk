c5b268f0119aeb6b79719cf7af86b8138f0c588a
Li Cheng <chengli1006@hotmail.com>
2015-12-01 01:56:53 +0100

    product bearbeiten

--- public/js/app.js
+++ public/js/app.js
@@ -10,45 +10,6 @@
 			
 		});
 		
-		this.products = [
-			{
-				id: 10001,
-				name: 'Aptamil pre',
-				price: 19000,
-				countBought: 0
-			},
-			{
-				id: 10002,
-				name: 'Aptamil 1',
-				price: 19000,
-				countBought: 0
-			},
-			{
-				id: 10003,
-				name: 'Aptamil 2',
-				price: 19000,
-				countBought: 0
-			},
-			{
-				id: 10004,
-				name: 'Aptamil 3',
-				price: 19000,
-				countBought: 0
-			},
-			{
-				id: 10005,
-				name: 'Aptamil 1+',
-				price: 19000,
-				countBought: 0
-			},
-			{
-				id: 10006,
-				name: 'Aptamil 2+',
-				price: 19000,
-				countBought: 0
-			}
-		];
-
 		this.boughtMoreThan0 = function (item) {
 			return item.countBought > 0;
 		};
--- resources/views/main.php
+++ resources/views/main.php
@@ -21,59 +21,78 @@
 		
 		<script src="js/app.js"></script>
     </head>
-    <body ng-app="ccmilk" ng-controller="ccmilkController as store">
-
-
+    <body ng-app="ccmilk" >
         <div class="container">
             <div class="jumbotron">
                 <h1>橙橙奶粉</h1>
             </div>
-			{{store.productList}}
-            <div class="row">
-                <div class="col-md-4">
+			
+            <div class="row" ng-controller="ccmilkController as store">			
+                <div class="col-md-6">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h3 class="panel-title">产品</h3>
                         </div>
                         <div style="height: 400px" class="panel-body">
-                            <div data-ng-repeat="milk in store.products">
-                                {{milk.name}} &nbsp;&nbsp;&nbsp;&nbsp;{{milk.price / 100 }}元
-                                <div class="btn-group" role="group" >
-                                    <button data-ng-click="milk.countBought = milk.countBought + 1" type="button" class="btn btn-default">+</button>
-                                    <button data-ng-click="milk.countBought > 0 ? milk.countBought = milk.countBought - 1 : 0" type="button" class="btn btn-default">-</button>
-                                </div>
-                            </div>
+							<div class="row" data-ng-repeat="product in store.productList">	
+								<div class="col-md-8">
+									{{product.brand}} {{product.specs}} {{product.pack_style}} {{product.weight}}g
+							
+								</div>
+								<div class="col-md-2">
+									{{product.price / 100 }}元
+							
+								</div>
+								<div class="col-md-2">
+									<div class="btn-group" role="group" >
+												<button class="btn btn-default btn-sm" ng-click="product.countBought = product.countBought + 1" type="button" class="btn btn-default">+</button>
+												<button class="btn btn-default btn-sm" ng-click="product.countBought > 0 ? product.countBought = product.countBought - 1 : 0" type="button" class="btn btn-default">-</button>
+											</div>
+                           
+							
+								</div>
+							</div>
+								<table class="table table-striped">
+									
+									<tr data-ng-repeat="product in store.productList">
+										<td>{{product.brand}} {{product.specs}} {{product.pack_style}} {{product.weight}}g</td>
+										<td>{{product.price / 100 }}元</td> 
+										<td><div class="btn-group" role="group" >
+												<button class="btn btn-default btn-sm" ng-click="product.countBought = product.countBought + 1" type="button" class="btn btn-default">+</button>
+												<button class="btn btn-default btn-sm" ng-click="product.countBought > 0 ? product.countBought = product.countBought - 1 : 0" type="button" class="btn btn-default">-</button>
+											</div>
+										</td>
+									</tr>
+								</table>
+                               
+                                
+                            
                             <button style="position: absolute; bottom: 40px" class="btn btn-default" type="submit">下单</button>
                         </div>
                     </div>
-
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h3 class="panel-title">货单</h3>
                         </div>
                         <div style="height: 400px" class="panel-body">
-                            <div data-ng-repeat="milk in store.products | filter : store.boughtMoreThan0">
-                                {{milk.name}} &nbsp;&nbsp;&nbsp;&nbsp;{{milk.price * milk.countBought / 100 }}元
+                            <div data-ng-repeat="product in store.productList | filter : store.boughtMoreThan0">
+                                {{product.brand}} {{product.specs}} {{product.pack_style}} {{product.weight}}g &nbsp;&nbsp;&nbsp;&nbsp;{{product.price * product.countBought / 100 }}元
                             </div>
 
                         </div>
                     </div>
-
-
-                </div>
-                <div class="col-md-4">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">历史订单</h3>
-                        </div>
-                        <div style="height: 400px" class="panel-body">
-
-                        </div>
-                    </div>
-                </div>
+                </div>               
             </div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">历史订单</h3>
+				</div>
+				<div style="height: 400px" class="panel-body">
+
+				</div>
+			</div>
         </div>
     </body>
 </html>
